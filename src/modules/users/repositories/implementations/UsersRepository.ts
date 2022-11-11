2e20559171998107a7f368aad4c386ff7f98474e
fattakbar <akbarfattahul22@gmail.com>
2022-11-11 23:06:39 +0700

    turn user into admin feature

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -32,7 +32,7 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
@@ -40,7 +40,16 @@ class UsersRepository implements IUsersRepository {
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const adminUser = Object.assign(receivedUser, {
+      admin: true,
+      updated_at: new Date(),
+    });
+
+    const index = this.users.indexOf(receivedUser);
+
+    if (index !== -1) this.users[index] = adminUser;
+
+    return adminUser;
   }
 
   list(): User[] {
